9038d428de2b32f4bb381d4def75cd7d03e914f0
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2024-04-03 13:17:21 +0530

    Time: 1314 ms (7.76%) | Memory: 11.9 MB (9.82%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    bool dfs(vector<vector<char>>& board,vector<vector<bool>>& visited, string word, int wordIndex,int i,int j){
+
+        if(i<0 || i>=visited.size() || j<0 || j>= visited[0].size() || visited[i][j] || board[i][j] != word[wordIndex])
+            return false;
+
+        if(wordIndex == word.size()-1)
+            return true;
+        
+        visited[i][j] = true;
+
+        bool found = dfs(board, visited, word, wordIndex + 1, i + 1, j) ||
+                     dfs(board, visited, word, wordIndex + 1, i - 1, j) ||
+                     dfs(board, visited, word, wordIndex + 1, i, j + 1) ||
+                     dfs(board, visited, word, wordIndex + 1, i, j - 1);
+
+        visited[i][j] = false;
+        return found;
+
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int rows = board.size();
+        if (rows == 0) return false;
+        int cols = board[0].size();
+
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (dfs(board, visited, word, 0, i, j))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
