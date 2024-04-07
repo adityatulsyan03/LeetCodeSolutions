4f62bc049952ee3cdc0b45ec2c26501b5670b3b3
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2024-04-07 18:14:04 +0530

    Time: 4 ms (9.36%) | Memory: 7.4 MB (39.07%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> st1,st2;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st1.push(i);
+            }
+            
+            if(s[i]=='*'){
+                st2.push(i);
+            }
+
+            if(s[i]==')'){
+                if(st1.size()!=0){
+                    st1.pop();
+                }
+                else if(st2.size()!=0){
+                    st2.pop();
+                }
+                else
+                    return false;
+            }
+        }
+        while(st1.size()!=0 && st2.size()!=0 && st1.top()<st2.top()){
+            st1.pop();
+            st2.pop();
+        }  
+        if(st1.size()==0)
+            return true;
+        return false;
+    }
+};
