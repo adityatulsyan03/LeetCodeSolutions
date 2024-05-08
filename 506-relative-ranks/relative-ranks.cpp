94aa13e183184cec940492b57f903f40aa807a74
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2024-05-08 22:19:25 +0530

    Time: 10 ms (51.87%) | Memory: 19.2 MB (5.21%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -3,12 +3,8 @@ public:
     vector<string> findRelativeRanks(vector<int>& score) {
         vector<int> s=score;
         sort(s.rbegin(),s.rend());
-        vector<string> ans;
-        for(int i=0;i<s.size();i++){
-            ans.push_back(to_string(i+1));
-        }
         unordered_map<int,string> m;
-        for(int i=0;i<ans.size();i++){
+        for(int i=0;i<s.size();i++){
             if (i == 0) m[s[i]] = "Gold Medal";
             else if (i == 1) m[s[i]] = "Silver Medal";
             else if (i == 2) m[s[i]] = "Bronze Medal";
