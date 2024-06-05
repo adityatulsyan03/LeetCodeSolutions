49ce79c4f917c49b47d4436bf0d74def60a8d147
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2024-06-05 20:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (11.36%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> ans(n+1,0);
+        ans[0]=1;
+        ans[1]=1;
+        for(int i=2;i<ans.size();i++)
+            ans[i] = ans[i-1] + ans[i-2];
+        return ans[n];
+    }
+};
