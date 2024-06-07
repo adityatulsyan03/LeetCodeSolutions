46fddc104bea55992b3ea7b0bc5aae3a046af00a
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2024-06-07 14:12:59 +0530

    Time: 91 ms (58.26%) | Memory: 80.9 MB (41.74%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,33 +1,65 @@
+class TrieNode {
+public:
+    char node;
+    unordered_map<char, TrieNode*> child;
+    bool isEnd;
+    TrieNode(char node) {
+        this->node = node;
+        isEnd = false;
+    }
+};
+
 class Solution {
 private:
-    vector<string> splitSentence(const string& sentence) {
-        vector<string> result;
-        istringstream iss(sentence);
-        string word;
-        while (iss >> word) {
-            result.push_back(word);
+    TrieNode* root;
+    
+    void buildTrie(string word) {
+        TrieNode* cur = root;
+        for (auto c : word) {
+            if (cur->child.find(c) == cur->child.end())
+                cur->child[c] = new TrieNode(c);
+            cur = cur->child[c];
         }
-        return result;
+        cur->isEnd = true;
     }
+
+    vector<string> splitString(string& s, char delimiter) {
+        vector<string> ans;
+        int start = 0;
+        int end = s.find(delimiter, start);
+        while (end != string::npos) {
+            string sub = s.substr(start, end - start);
+            ans.push_back(sub);
+            start = end + 1;
+            end = s.find(delimiter, start);
+        }
+        string sub = s.substr(start);
+        ans.push_back(sub);
+        return ans;
+    }
+
+    string queryTrie(string word) {
+        TrieNode* cur = root;
+        string s = "";
+        for (auto c : word) {
+            if (cur->child.find(c) == cur->child.end()) return word;
+            cur = cur->child[c];
+            s += c;
+            if (cur->isEnd) return s;
+        }
+        return word;
+    }
+
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        map<string,int> cnt;
-        for(string i:dictionary)
-            cnt[i]=1;
-        string ans="";
-        dictionary = splitSentence(sentence);
-        for(string i:dictionary){
-            string temp="";
-            for(char j:i){
-                temp+=j;
-                if(cnt[temp]>0 || temp==i){
-                    ans+= temp;
-                    break;
-                }
-            }
-            ans+=" ";
+        root = new TrieNode('/');
+        for (auto word : dictionary) buildTrie(word);
+
+        vector<string> splitSentences = splitString(sentence, ' ');
+        string ans = "";
+        for (int i = 0; i < splitSentences.size(); i++) {
+            ans += queryTrie(splitSentences[i]) + ((i != splitSentences.size() - 1) ? " " : "");
         }
-        ans.pop_back();
         return ans;
     }
 };
