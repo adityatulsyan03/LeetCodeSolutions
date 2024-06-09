d9e5a27d9e0c14513bb012d8fa548f380211502f
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2024-06-09 16:44:48 +0530

    Time: 176 ms (66.72%) | Memory: 134.7 MB (32.00%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int, int> remainderMap;
-        int cumulativeSum = 0;
-        
-        // Step 1: Initialize Remainder Map
-        remainderMap[0] = -1;
-        
-        // Step 2: Iterate Through the List
-        for (int i = 0; i < n; i++) {
-            // Step 3: Calculate Cumulative Sum
-            cumulativeSum += nums[i];
-            
-            // Step 4: Calculate Remainder
-            int remainder = k == 0 ? cumulativeSum : cumulativeSum % k;
-            
-            // Step 5: Update Remainder Map
-            if (remainderMap.count(remainder)) {
-                // Check if segment length is at least two
-                if (i - remainderMap[remainder] > 1) {
-                    return true;
-                }
-            } else {
-                remainderMap[remainder] = i;
+        int n=nums.size(),prefsum=0;
+        unordered_map<int,int> firstocc;
+        firstocc[0]=0;
+        for(int i=0;i<n;i++){
+            prefsum = (prefsum+nums[i])%k;
+            if(firstocc.find(prefsum)!=firstocc.end()){
+                if(i+1 - firstocc[prefsum] >= 2)  return true;
             }
+            else
+                firstocc[prefsum]=i+1;
         }
-        
-        // Step 6: Return the Result
         return false;
     }
 };
