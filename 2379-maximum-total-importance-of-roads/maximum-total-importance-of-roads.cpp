2d5b2bacb64d3fbd447caffa1f77bd40a771d867
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2024-06-28 16:31:45 +0530

    Time: 334 ms (41.90%) | Memory: 134.7 MB (46.38%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        vector<int> degree(n,0);
-        for(auto edge:roads){
-            degree[edge[0]]++;
-            degree[edge[1]]++;
+        unordered_map<int,int> cnt;
+        for(int i=0;i<roads.size();i++){
+            cnt[roads[i][0]]++;
+            cnt[roads[i][1]]++;
         }
-        long long ans=0,v=1;
-        sort(degree.begin(),degree.end());
-        for(auto i:degree){
-            ans+=i*v;
-            v++;
+        priority_queue<pair<int,int>> pq;
+        for(auto [i,j]:cnt)
+            pq.push({j,i});
+        long long ans=0,v=n;
+        while (!pq.empty()) {
+            pair<int, int> topElement = pq.top();
+            ans+= v*topElement.first;
+            v--;
+            pq.pop();
         }
         return ans;
     }
