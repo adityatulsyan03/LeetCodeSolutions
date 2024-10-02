5f30e3d946b57f1c65e93cabc988419fa7970280
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2024-10-03 02:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (85.50%) - LeetSync

--- 1935-minimum-number-of-operations-to-reinitialize-a-permutation/minimum-number-of-operations-to-reinitialize-a-permutation.cpp
+++ 1935-minimum-number-of-operations-to-reinitialize-a-permutation/minimum-number-of-operations-to-reinitialize-a-permutation.cpp
@@ -1,31 +1,11 @@
 class Solution {
-private:
-    bool equal(vector<int> arr,vector<int> pern){
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]!=pern[i])
-                return 0;
-        }
-        return 1;
-    }
 public:
     int reinitializePermutation(int n) {
-        vector<int> pern;
-        for(int i=0;i<n;i++)    pern.push_back(i);
-        vector<int> prev=pern;
+        int i=1,j=1,k=1;
         for(int t=0;t<1000;t++){
-            vector<int> arr;
-            for(int i=0;i<n;i++){
-                if(i%2==0){
-                    arr.push_back(prev[i/2]);
-                }
-                else{
-                    arr.push_back(prev[n/2 + (i-1)/2]);
-                }
-            }
-            prev=arr;
-            if(equal(prev,pern)){
-                return t+1;
-            }
+            if(k%2==0)  k=k/2;
+            else    k=(n/2)+(k-1)/2;
+            if(k==i)    return t+1;
         }
         return -1;
     }
