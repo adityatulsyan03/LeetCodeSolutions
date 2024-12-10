3737343a169d3b253347cb9e4f49edfcda7ff499
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2024-12-10 21:23:39 +0530

    Time: 3 ms (61.13%) | Memory: 8.2 MB (5.39%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,19 @@
 class Solution {
+private:
+    void fill(vector<int>& mem,int n){
+        if(mem[n]!=0){
+            return;
+        }
+        if(n-1>0)   fill(mem,n-1);
+        if(n-2>0)   fill(mem,n-2);
+        mem[n]=mem[n-1]+mem[n-2];
+    }
 public:
     int fib(int n) {
-        if(n==0 || n==1)
-            return n;
-        return fib(n-1)+fib(n-2);
+        vector<int> mem(n+1,0);
+        if (n >= 1) mem[1] = 1;
+        if (n == 0) return 0;
+        fill(mem,n);
+        return mem[n];
     }
 };
