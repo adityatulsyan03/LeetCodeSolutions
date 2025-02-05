986555eb9765b32aa3df023cf49aefe8c6c010ec
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2025-02-05 22:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (61.87%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,30 +1,11 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        if(s1.size()!=s2.size()){
-            return false;
-        }
-        char diff='/';
-        char diff2='/';
-        int c=0;
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]!=s2[i]){
-                if(diff=='/'){
-                    diff2=s1[i];
-                    c++;
-                    diff=s2[i];
-                }else{
-                    if(c>1){
-                        return false;
-                    }
-                    if(s1[i]==diff && s2[i]==diff2){
-                        c++;
-                    }else{
-                        return false;
-                    }
-                }
-            }
-        }
-        return c==2||c==0?true:false;
+        vector<int> diff;
+        for (int i = 0; i < s1.size(); i++)
+            if (s1[i] != s2[i]) diff.push_back(i);
+        
+        return diff.size() == 0 || (diff.size() == 2 && 
+            s1[diff[0]] == s2[diff[1]] && s1[diff[1]] == s2[diff[0]]);
     }
 };
