dca06ec520cc238af2bab2074166ec4ecb41d43b
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2025-05-12 12:51:28 +0530

    Time: 16 ms (41.55%) | Memory: 14 MB (49.57%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
+        unordered_map<int,int> cnt;
+        for(int i:digits){
+            if(cnt[i]==3){
+                continue;
+            }else{
+                cnt[i]++;
+            }
+        }
+        vector<int> digit;
+        for(auto [i,j]:cnt){
+            for(int k=0;k<j;k++){
+                digit.push_back(i);
+            }
+        }
         set<int> s;
-        for(int i=0;i<digits.size();i++){
-            if(digits[i]%2==0){
-                for(int j=0;j<digits.size();j++){
+        for(int i=0;i<digit.size();i++){
+            if(digit[i]%2==0){
+                for(int j=0;j<digit.size();j++){
                     if(j==i) continue;
-                    for(int k=0;k<digits.size();k++){
-                        if(k==i || k==j || digits[k]==0) continue;
-                        int t=digits[k]*100;
-                        t+=digits[j]*10;
-                        t+=digits[i];
+                    for(int k=0;k<digit.size();k++){
+                        if(k==i || k==j || digit[k]==0) continue;
+                        int t=digit[k]*100;
+                        t+=digit[j]*10;
+                        t+=digit[i];
                         s.insert(t);
                     }
                 }
