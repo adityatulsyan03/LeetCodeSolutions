f15503ae0207a30bcdb3d2c0c58fd94d1e15b132
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2025-06-07 22:53:00 +0530

    Time: 176 ms (28.57%) | Memory: 63.3 MB (20.45%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -3,21 +3,20 @@ public:
     string clearStars(string s) {
         int n=s.size();
         vector<bool> vis(n,true);
-        unordered_map<char,set<int>> idx;
+        unordered_map<char,vector<int>> idx;
         set<char> ch;
 
         for(int i=0;i<n;i++){
             if(s[i]!='*'){
-                idx[s[i]].insert(i);
+                idx[s[i]].push_back(i);
                 ch.insert(s[i]);
                 continue;
             }
             vis[i]=false;
             char smallest = *ch.begin();
-            cout<<smallest<<endl;
-            int lastIdx = *idx[smallest].rbegin();
-            vis[lastIdx]=false;
-            idx[smallest].erase(lastIdx);
+            int lastIdx = idx[smallest].back();
+            vis[lastIdx] = false;
+            idx[smallest].pop_back();
             if(idx[smallest].size()==0){
                 ch.erase(smallest);
             }
