cb48f0e2ee96cb131b4fe66bbfd0817b690b560d
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2025-08-12 21:32:21 +0530

    Time: 273 ms (42.26%) | Memory: 274.4 MB (28.10%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,19 +1,32 @@
 class Solution {
 private:
     const int MOD = 1000000007;
-    int helper(int n, int x, int num, int curr, vector<vector<int>>& dp){
-        if(curr == n)  return 1;
-        if(curr > n || pow(num,x)>n)   return 0;
-        if(dp[num][curr] != -1) return dp[num][curr];
-
-        int take = helper(n,x,num+1,curr+pow(num,x),dp);
-        int notTake = helper(n,x,num+1,curr,dp);
-
-        return dp[num][curr] = (take + notTake)%MOD;
-    }
 public:
     int numberOfWays(int n, int x) {
-        vector<vector<int>> dp(n+2,vector<int>(n+1,-1));
-        return helper(n,x,1,0,dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        
+        dp[0][0] = 1;
+
+        for (int num = 1; num <= n; num++) {
+            long long p = 1;
+            for (int i = 0; i < x; i++) {
+                p *= num;
+                if (p > n) break;
+            }
+            if (p > n) {
+                for (int sum = 0; sum <= n; sum++)
+                    dp[num][sum] = dp[num - 1][sum];
+                continue;
+            }
+            for (int sum = 0; sum <= n; sum++) {
+                dp[num][sum] = (dp[num][sum] + dp[num - 1][sum]) % MOD;
+
+                if (sum >= p) {
+                    dp[num][sum] = (dp[num][sum] + dp[num - 1][sum - p]) % MOD;
+                }
+            }
+        }
+
+        return dp[n][n];
     }
 };
