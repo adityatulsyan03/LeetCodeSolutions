a8c17b571137b02e5be60a43b84cc82ee85f569a
Aditya Tulsyan <112763520+adityatulsyan03@users.noreply.github.com>
2025-08-14 12:19:36 +0530

    Time: 1089 ms (5.05%) | Memory: 261.4 MB (5.00%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,27 +1,24 @@
 class Solution {
+private:
+    void helper(int n,unordered_set<string>& ans,string temp){
+        if(temp.size()==n*2){
+            ans.insert(temp);
+        }
+        else{
+            string a="()";
+            for(int i=0;i<=temp.size();i++){
+                string t=temp;
+                t.insert(i,a);
+                helper(n,ans,t);
+            }
+        }
+    }
 public:
     vector<string> generateParenthesis(int n) {
-        
-        vector<string> res;
-
-        dfs(0, 0, n, res, "");
-
-        return res;
-    }
-
-    void dfs(int l, int r, int t, vector<string>& res, string s) {
-        if(l==r && (l+r) == t*2) {
-            res.push_back(s);
-            return;
-        }
-
-        if(l<t) {
-            dfs(l+1, r, t, res, s + '(');
-        }
-
-        if(r<l) {
-            dfs(l, r+1, t, res, s + ')');
-        }
-
+        unordered_set<string> a;
+        string temp="";
+        helper(n,a,temp);
+        vector<string> ans(a.begin(),a.end());
+        return ans;
     }
 };
